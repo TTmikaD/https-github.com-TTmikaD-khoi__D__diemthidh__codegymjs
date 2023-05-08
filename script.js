73f806710fcc4bac31ba04c388ecfe4681aff357
TTmikaD <101580923+TTmikaD@users.noreply.github.com>
2023-05-08 21:42:25 +0700

    2 files modified

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 function click_caculator() {
-  
+  let point__math = document.getElementById('row_math').value;
+  let point__literature = document.getElementById('row_literature').value;
+  let point__english = document.getElementById('row_english').value;
+
+  let general__caculator =
+    parseInt(point__math) +
+    parseInt(point__literature) +
+    parseInt(point__english);
+
+  switch (location) {
+    case KV1:
+      document.getElementById('show__result').innerHTML =
+        general__caculator + 0.75;
+      break;
+    case KV2__NT:
+      document.getElementById('show__result').innerHTML =
+        general__caculator + 0.5;
+    case KV2:
+      document.getElementById('show__result').innerHTML =
+        general__caculator + 0.25;
+    case KV3:
+      document.getElementById('show__result').innerHTML =
+        general__caculator + 0;
+  }
 }
